5b7bb499f1fdd5b4adba4699eeb80898cc34f584
KavyaPremaseelan <kavyapremaseelan@gmail.com>
2024-05-05 18:36:55 +0530

    mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://kavyapremaseelan:Kavya2003@cluster0.i8ipqdx.mongodb.net/mern-fooddelivery');
+        await mongoose.connect(process.env.MONGO);
         console.log("DB connected");
     } catch (error) {
         console.error("Error connecting to database:", error);
