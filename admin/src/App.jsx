89baa6c37e7b8d7993a788a81ebe0ab3ad401316
KavyaPremaseelan <kavyapremaseelan@gmail.com>
2024-05-17 16:11:29 +0530

    order display in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         <Routes>
           <Route path='/add' element={<Add url={url}/>}/>
           <Route path='/list' element={<List url={url}/>}/>
-          <Route path='/orders' element={<Orders/>}/>
+          <Route path='/myorders' element={<Orders url={url}/>}/>
         </Routes>
       </div>
     </div>
